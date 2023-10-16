f3930e6978612371bd2213029360f946f7297c54
Gustavo <140539608+Luxipe@users.noreply.github.com>
2023-10-16 14:48:04 -0300

    Update Bolo17.java

--- Bolo17.java
+++ Bolo17.java
@@ -1,14 +1,15 @@
-package; Bolos;
+package Bolos;
 
 import java.util.Scanner;
 
 public class Bolo17 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Entre com o número de carbonos na cadeia carbônica: ");
-        int numCarbonos = scanner.nextInt();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Entre com o número de carbonos na cadeia carbônica: "); //digita no terminal Entre com o número de carbonos na cadeia carbônica: (numero).
+            int numCarbonos = scanner.nextInt(); // função do int numCarbonos , scanner.nextInt() = é usado para escanear ou analisar a entrada.
 
-        int numCarbonosPrimarios = numCarbonos - 2;
-        System.out.println("A cadeia possui " + numCarbonosPrimarios + " carbonos primários.");
+            int numCarbonosPrimarios = numCarbonos - 2; // Int numCarbonosPrimarios = numCarbonos = vai ser -2
+            System.out.println("A cadeia possui " + numCarbonosPrimarios + " carbonos primários."); // o resultado final vai mostrar a cadeia (numero) carbonosPrimários.
+        }
     }
 }
