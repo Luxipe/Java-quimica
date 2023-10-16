d291146ae853a02f199eaafb25f8cfaf2db54b05
Gustavo <140539608+Luxipe@users.noreply.github.com>
2023-10-16 14:42:40 -0300

    Update Bolo16.java

--- Bolo16.java
+++ Bolo16.java
@@ -1,14 +1,15 @@
-package; Bolos;
+package Bolos;
 
 import java.util.Scanner;
 
 public class Bolo16 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Entre com um número real: ");
-        double numero = scanner.nextDouble();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Entre com um número real: "); // o usuario vai colocar um número real.
+            double numero = scanner.nextDouble(); // uma função double numero = Escaneia o próximo TOKEN de um input e retorna como double
 
-        double valorAbsoluto = Math.abs(numero);
-        System.out.println("O valor absoluto de " + numero + " é " + valorAbsoluto + ".");
+            double valorAbsoluto = Math.abs(numero); // função double valorAbsoluto = Retorna o valor absoluto de um número (numero).
+            System.out.println("O valor absoluto de " + numero + " é " + valorAbsoluto + ".");// o resultado final vai falar pro usuario o valor Absoluto do numero que ele escolher.
+        }
     }
 }
