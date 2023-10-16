d66ba64ca4a6fc48c5d694dab0230e6e8f924e39
Gustavo <140539608+Luxipe@users.noreply.github.com>
2023-10-16 14:36:00 -0300

    Update Bolo15.java

--- Bolo15.java
+++ Bolo15.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Bolo15 {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Entre com um número real não negativo: ");
-            double numero = scanner.nextDouble();
+            System.out.print("Entre com um número real não negativo: "); // vai digitar no terminal Entre com um número real não negativo: (numero) o usuario
+            double numero = scanner.nextDouble(); // função double numero = resultado Escaneia o próximo TOKEN de um input e retorna como double.
 
-            if (numero >= 0) {
-                double raizQuadrada = Math.sqrt(numero);
-                System.out.println("A raiz quadrada de " + numero + " é " + raizQuadrada + ".");
+            if (numero >= 0) { // maior igual a 0.
+                double raizQuadrada = Math.sqrt(numero); // função double raiz quadrada = (numero)
+                System.out.println("A raiz quadrada de " + numero + " é " + raizQuadrada + "."); // a raiz quadrada de (numero) é a raiz quadrada.
             } else {
-                System.out.println("O número não é não negativo.");
+                System.out.println("O número não é não negativo."); // digita no terminal O número não é não negativo quando o resultado do terminal for ( numero negativo).
             }
         }
     }
