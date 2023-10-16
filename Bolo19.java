4de5157d38e5c61aa78cddadb10e80251a47c33b
Gustavo <140539608+Luxipe@users.noreply.github.com>
2023-10-16 15:01:28 -0300

    Update Bolo19.java

--- Bolo19.java
+++ Bolo19.java
@@ -1,16 +1,17 @@
-package; Bolos;
+package Bolos;
 
 import java.util.Scanner;
 
 public class Bolo19 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Entre com a configuração eletrônica do átomo: ");
-        String configuracaoEletronica = scanner.nextLine();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Entre com a configuração eletrônica do átomo: "); // Entre com aconfiguração eletrônica do átomo o usuario vai colocar  o (numero).
+            String configuracaoEletronica = scanner.nextLine();
 
-        int posicaoUltimoSubnivel = configuracaoEletronica.lastIndexOf(" ");
-        String ultimoSubnivel = configuracaoEletronica.substring(posicaoUltimoSubnivel);
+            int posicaoUltimoSubnivel = configuracaoEletronica.lastIndexOf(" ");
+            String ultimoSubnivel = configuracaoEletronica.substring(posicaoUltimoSubnivel + 1);
 
-        System.out.println("O último subnível preenchido é " + ultimoSubnivel);
+            System.out.println("O último subnível preenchido é " + ultimoSubnivel); //o número que o usuario escolheu no terminal vai mostrar O ultimo subnivel preenchido é (numero).
+        }
     }
 }
