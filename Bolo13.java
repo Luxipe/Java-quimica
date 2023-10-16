1319fb9c0c35aa034bc37c4fb8c125c2dc9c4f8c
Gustavo <140539608+Luxipe@users.noreply.github.com>
2023-10-16 13:52:24 -0300

    Update Bolo13.java

--- Bolo13.java
+++ Bolo13.java
@@ -1,14 +1,15 @@
-package; Bolos;
+package Bolos;
 
 import java.util.Scanner;
 
 public class Bolo13 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Entre com o comprimento da aresta do cubo em cm: ");
-        double aresta = scanner.nextDouble();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Entre com o comprimento da aresta do cubo em cm: ");
+            double aresta = scanner.nextDouble();
 
-        double volume = aresta * aresta * aresta;
-        System.out.println("O volume do cubo é " + volume + " cm³.");
+            double volume = aresta * aresta * aresta;
+            System.out.println("O volume do cubo é " + volume + " cm³.");
+        }
     }
 }
